ed0df96354379757154082e10717fe268f8cbd67
Kabir Deula <kabirdeula167@gmail.com>
2025-06-16 11:33:36 +0545

    feat: ✨Add game resources management and rendering logic

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,22 @@
+import { resources } from "./Resources";
 import "./style.css";
+
+const canvas = document.querySelector("#game-canvas") as HTMLCanvasElement;
+
+const ctx = canvas?.getContext("2d");
+
+const draw = () => {
+  const sky = resources.images.sky;
+  if (sky.isLoaded) {
+    ctx?.drawImage(sky.image, 0, 0);
+  }
+
+  const ground = resources.images.ground;
+  if (ground.isLoaded) {
+    ctx?.drawImage(ground.image, 0, 0);
+  }
+};
+
+setInterval(() => {
+  draw();
+}, 300);
