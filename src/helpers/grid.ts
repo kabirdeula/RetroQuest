61661e4333f6072c5ec4358dd0522871890ae92d
Kabir Deula <kabirdeula167@gmail.com>
2025-06-18 10:37:39 +0545

    feat: ✨Implement wall collision detection and add Level1 walls

--- src/helpers/grid.ts
+++ src/helpers/grid.ts
@@ -1,3 +1,10 @@
 export const gridCells = (n: number) => {
   return n * 16;
 };
+
+export const isSpaceFree = (walls: Set<any>, x: number, y: number) => {
+  const str = `${x},${y}`;
+  const isWallPresent = walls.has(str);
+
+  return !isWallPresent;
+};
